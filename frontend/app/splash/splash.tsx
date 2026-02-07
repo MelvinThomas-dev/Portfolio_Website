ab6e65dd28cea7e7b293a8faecfca8600501bd3a
MelvinThomas-dev <melthomas220@gmail.com>
2026-02-07 21:17:58 +0530

    Tailwind css configuration

---
+++ frontend/app/splash/splash.tsx
@@ -0,0 +1,45 @@
+import "tailwindcss";
+
+
+
+export default function spalsh() {
+    return (
+      
+        <>
+            
+        <div className="h-screen flex items-center justify-center bg-blue-500 border-4 border-red-500"> <p className="text-white text-2xl">Tailwind is working!</p> </div>
+
+
+
+        {/* <div className="flex justify-center ">
+                <div >1</div>
+                <div >2</div>
+                <div >3</div>
+        </div> */}
+
+
+{/* 
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          Welcome To  My
+              </h1>
+              <h1 className="text-2xl font-bold text-black dark:text-zinc-50">
+                    Portfolio Website
+              </h1>
+      </main>
+    </div> */}
+
+
+
+
+
+
+
+
+
+</>
+
+        
+  );
+}
